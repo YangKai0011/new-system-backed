064baef2ca976c64a729d75801c8d9dc72db3662
yuki <443926471@qq.com>
2020-04-08 21:01:01 +0800

    接口数值改变

--- routes/students.js
+++ routes/students.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 const StudentCurd = require('../model/StudentCurd');
 
-router.get('/student', function (req, res, next) {
+router.get('/', function (req, res, next) {
   const param = req.query;
   console.log(param);
   if (param.type === 'search') {
     switch (param.mold) {
-      case '1':
+      case '0':
         //通过年级，专业，或二者组合来查找楼号和宿舍号
         if (!param.grade && !param.profession) {
           res.send('no found right query');
@@ -16,7 +16,7 @@ router.get('/student', function (req, res, next) {
           StudentCurd.findGradeAndProfession(param).then(data(req, res));
           break;
         }
-      case '2':
+      case '1':
         //通过宿舍号，楼号或二者组合来查找学生信息
         if (!param.dormitoryNumber && !param.buildNumber) {
           res.send('no found right query');
@@ -25,20 +25,23 @@ router.get('/student', function (req, res, next) {
           StudentCurd.findDormitory(param).then(data(req, res));
           break;
         }
-      case '3':
+      case '2':
         if (param.grade || param.profession || param.department) {
           //通过宿管号,年级,专业系别
           StudentCurd.findStub(param).then(data(req, res));
           break;
-        } else if (param.dormitoryNumber) {
-          //通过宿管号，宿舍号查询
-          StudentCurd.findStubAndDormitoryNumber(param).then(data(req, res));
-          break;
-        } else if (param.name || param.studentNumber) {
-          //通过学号姓名查询
-          StudentCurd.findStubNameAndId(param).then(data(req, res));
-          break;
-        }
+        } 
+        case '3':
+          if (param.dormitoryNumber) {
+            //通过宿管号，宿舍号查询
+            StudentCurd.findStubAndDormitoryNumber(param).then(data(req, res));
+            break;
+          } else if (param.name || param.studentNumber) {
+            //通过学号姓名查询
+            StudentCurd.findStubNameAndId(param).then(data(req, res));
+            break;
+          }
+          
       default:
         res.send('本次请求未正确发送');
         break;
