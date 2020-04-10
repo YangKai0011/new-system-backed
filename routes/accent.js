1c0dcf8f4fe026843b1f2a016a4ae99289f8cfca
yuki <443926471@qq.com>
2020-04-10 13:14:02 +0800

    身份登录

--- routes/accent.js
+++ routes/accent.js
@@ -17,16 +17,22 @@ router.post('/login', function (req, res, next) {
       };
       const token = tokenUtil.createToken(userInfo);
       //TODO 
-      res.json({ key: token },{role: user.role},{accent: user.accent});
+      res.json({ key: token });
     }
   });
 });
 
 router.get('/check', function (req, res, next) {
   let token = req.headers['authorization'];
+  //token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiYWNjZW50Ijoic3R1YjEiLCJyb2xlIjoic3R1YiIsImlhdCI6MTU4NjQ5NTU0NSwiZXhwIjoxNTg2NTgxOTQ1fQ.LT1R9leyr58JmG09O03IDUneiQ0T0-zoNsVZo0i53sY'
   if (token) {
     const result = tokenUtil.checkToken(token);
-    res.json({ status: result },{role: token.role});
+    if (result[0]) {
+      res.json({ status: result[0], role: result[1].role });
+    } else {
+      res.json({ status: false });
+    }
+    //res.json({ status: result[0], role: result[1].role});
   } else {
     res.json({ status: false });
   }
