10529aac5f44625e1ab6fabfc0febac4c255dac8
yuki <443926471@qq.com>
2020-04-12 00:47:04 +0800

    上传excel文件

--- routes/students.js
+++ routes/students.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 const StudentCurd = require('../model/StudentCurd');
 const multer = require('multer');
+const fs = require('fs');
 const excel = require('../lib/excel-utils');
 router.get('/', function (req, res, next) {
   const param = req.query;
   console.log(param);
   if (param.type === 'search') {
-    if (param.role === '学工部') {
+    if (param.role === 'Controller') {
       if (param.grade || param.profession) {
         //按年级专业查询
         StudentCurd.findGradeAndProfession(param).then(data(req, res));
@@ -15,14 +16,14 @@ router.get('/', function (req, res, next) {
         //按楼号宿舍号查询
         StudentCurd.findDormitory(param).then(data(req, res));
       }
-    } else if (param.role === '导员') {
+    } else if (param.role === 'Instructor') {
       if (param.grade || param.profession || param.department) {
         //按年级系别专业查询
         StudentCurd.findGradeProfessionDepartment(param).then(data(req, res));
       } else {
         StudentCurd.findDormitory(param).then(data(req, res));
       }
-    } else if (param.role === '宿管') {
+    } else if (param.role === 'House') {
       if (param.dormitoryNumber) {
         //通过宿舍号查询
         StudentCurd.findStubAndDormitoryNumber(param).then(data(req, res));
@@ -38,41 +39,41 @@ router.get('/', function (req, res, next) {
   } */ else if (param.type === 'delete') {
     //删除信息
     StudentCurd.deleteByStudentNumber(param).then(data(req, res));
-  } /* else if (param.type === 'insert') {//导员导入信息
-    excel('text.xlsx', function (data) {
+  }
+});
+
+//导员批量导入信息
+router.post('/insert', multer({
+  dest: 'public/xlsx'
+}).single('file'), function (req, res, next) {
+  if (req.file.length === 0) {
+    return res.json({ error: '上传文件不能为空' });
+  } else {
+    let file = req.file;
+    fs.renameSync('./public/xlsx/' + file.filename, './public/xlsx/' + file.originalname);
+    excel(file.originalname, function (data) {
       if (!data.err) {
         for (let i in data) {
           let arr = [];
           for (let j in Object.values(data[i])) {
             arr[j] = Object.values(data[i])[j];
           }
-          StudentCurd.insertByInstruct(arr, function(err){
-            if(err){
+          StudentCurd.insertByInstruct(arr, function (err) {
+            if (err) {
               res.send(err);
             }
           });
-          res.send('success');
         }
+        res.send('文件上传成功并且导入成功');
       } else {
-        console.log(data.err);
+        res.json(data.err);
       }
     });
-  } */
+  }
 });
 
-//导员批量导入信息
-/* router.post('/insert', multer({
-  dest: 'public/xlsx'
-}).single('file'), function(req, res, next){
-  if(req.file.length === 0){
-    return res.json({error : '上传文件不能为空'});
-  }else{
-    res.json('success');
-  }
-}) */
-
 //导员修改信息
-router.post('/update', function(req, res){
+router.post('/update', function (req, res) {
   StudentCurd.updateMessage(param).then(data(req, res));
 });
 
@@ -88,21 +89,21 @@ function data(req, res) {
     let arr = [];
     if (!data.err) {
       const results = data.results;
-        if(req.query.type === 'search'){
+      if (req.query.type === 'search') {
         for (let i = 0; i < results.length; i++) {
-          if (req.query.role === '学工部') {
+          if (req.query.role === 'Controller') {
             if (req.query.grade || req.query.profession) {
               var map = { 楼号: null, 宿舍号: null };
             } else {
               var map = { 姓名: null, 系名: null, 专业: null, 年级: null, 电话: null, 导员姓名: null, 导员电话: null, 宿舍长: null, 宿舍长电话: null };
             }
-          } else if (req.query.role === '导员') {
+          } else if (req.query.role === 'Instructor') {
             if (req.query.grade || req.query.profession || req.query.department) {
               var map = { 楼号: null, 宿舍号: null };
             } else {
               var map = { 学号: null, 姓名: null, 系名: null, 专业: null, 年级: null, 班级: null, 电话: null, 父亲电话: null, 母亲电话: null };
             }
-          } else if (req.query.role === '宿管') { var map = { 学号: null, 姓名: null, 系名: null, 专业: null, 年级: null, 班级: null, 电话: null, 导员姓名: null, 导员电话: null, 宿舍号: null, 宿舍长: null, 宿舍长电话: null, 父亲电话: null, 母亲电话: null }; }
+          } else if (req.query.role === 'House') { var map = { 学号: null, 姓名: null, 系名: null, 专业: null, 年级: null, 班级: null, 电话: null, 导员姓名: null, 导员电话: null, 宿舍号: null, 宿舍长: null, 宿舍长电话: null, 父亲电话: null, 母亲电话: null }; }
 
           for (let j = 0; j < Object.values(results[i]).length; j++) {
             map[Object.keys(map)[j]] = Object.values(results[i])[j];
@@ -110,7 +111,7 @@ function data(req, res) {
           arr[i] = map;
         }
         res.json(arr);
-      }else {
+      } else {
         results.affectedRows === 0 ? res.json('false') : res.json('success');
       }
     } else {
